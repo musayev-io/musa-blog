0af0848c9bf86e0ae8df1b952cdc61e9417dc3c4
Musa Musayev <musa@musayev.io>
2020-10-25 20:19:29 -0500

    Initialize project and render Prismic's Home_Hero_Group I created

---
+++ layouts/Base.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <PrismicHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import PrismicHeader from '~/components/prismic/PrismicHeader'
+
+export default {
+  components: {
+    PrismicHeader,
+  },
+  head() {
+    return {
+      title: 'Prismic Nuxt.js Multi Page Website',
+    }
+  },
+  // // Called before rendering the layout (even for error page)
+  // async middleware({ store, $prismic }) {
+  //   await store.dispatch('fetchMenu', $prismic)
+  // },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <!-- <PrismicHomeBanner /> -->
+    <!-- <p v-if="$fetchState.pending">Pending...</p>
+    <p v-else-if="$fetchState.error">error...</p>
+    <p v-else>{{ homepage }}</p> -->
+    <!-- <p>{{ homepage }}</p> -->
+    <PrismicHomeBanner :hero="hero" />
+  </section>
+</template>
+
+<script>
+import PrismicHomeBanner from '~/components/prismic/PrismicHomeBanner.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    PrismicHomeBanner,
+  },
+  async fetch() {
+    this.home = (await this.$prismic.api.getSingle('home')).data
+    this.hero = this.home.hero_group[0]
+  },
+  data() {
+    return {
+      home: {},
+      hero: {},
+    }
+  },
+  mounted() {
+    console.log(JSON.stringify(this.home))
+    console.log(JSON.stringify(this.hero))
+  },
+}
+</script>
