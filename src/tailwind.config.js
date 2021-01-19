3e755893cdfd4221815ed6082470aa256b62a5c0
Musa Musayev <musa@musayev.io>
2021-01-18 18:22:44 -0600

    Delete ia-writer fonts

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -6,7 +6,6 @@
  */
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
-const { emerald } = require('tailwindcss/colors')
 
 module.exports = {
   theme: {
